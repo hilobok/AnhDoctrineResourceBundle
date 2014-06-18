3bed998200bccb2930546b9dbf8e1a5bfdf6a46c
Andrew Hilobok <hilobok@gmail.com>
2014-06-18 13:12:52 +0300

    Removed unused use statement

--- DependencyInjection/AnhDoctrineResourceExtension.php
+++ DependencyInjection/AnhDoctrineResourceExtension.php
@@ -6,14 +6,13 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class AnhDoctrineResourceExtension extends Extension // implements PrependExtensionInterface
+class AnhDoctrineResourceExtension extends Extension
 {
     /**
      * {@inheritDoc}
